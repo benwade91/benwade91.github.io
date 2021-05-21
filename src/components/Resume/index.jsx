17371b38540128e9447857e16d9d3e3327482e08
benwade91 <70988204+benn925@users.noreply.github.com>
2021-05-21 10:58:42 -0700

    updated resume

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './style.css'
 const Resume = () => {
     return <div className='resumeBody'>
-        <a href="https://docs.google.com/document/d/1s2n5qTbV2ZZS1qKAZmlodEvU8re8H9vXfvWUXXDVLgA/export?format=pdf" className='fa fa-download fa-3x dlLink' download> </a>
-        <iframe title='resume' src="https://docs.google.com/document/d/e/2PACX-1vSELbG2XkU8VGcopmlH20FPsoHQYuypq4Ojb0SdCaWmpletgWKmtlxZo6pncs0UrBJMsk4Q4a-UFuhj/pub?embedded=true"></iframe>
+        <a href="https://docs.google.com/document/d/1ootXX70hbul_e4mmBJ8YcKpFB5cORiPHgHUbO-G_xmw/export?format=pdf" className='fa fa-download fa-3x dlLink' download> </a>
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vQMHWoihZr17j_d3gEu71AQsX60pD3xQxqW4KYh_5L1ytoUYZl5_v3C9TK2ti9iOE2P_cWXVePaSiFK/pub?embedded=true"></iframe>
   
     </div>
 }
